61ccb9f1f5289d0bd8a716a6c795ab9910e2b925
Gordin508 <5062634+Gordin508@users.noreply.github.com>
2022-10-12 19:40:43 +0200

    Add LOD generator

--- script.py
+++ script.py
@@ -18,11 +18,42 @@ bl_info = {
     "name": "TMTK Tools",
     "blender": (3, 0, 0),
     "category": "Object",
+    "version": (0, 1),
+    "description": "Tools to make TMTK item creation easier"
 }
 """
 
+class TMTKLODGenerator(bpy.types.Operator):
+    bl_idname = "object.lodoperator"
+    bl_label = "TMTK Create LODs"
+
+    @classmethod
+    def poll(cls, context):
+        return (context.active_object and bpy.context.active_object.type == "MESH")
+
+    def execute(self, context):
+        mesh = context.active_object
+        if (mesh.name.endswith("_L0")):
+            mesh.name = mesh.name.replace("_L0", "")
+
+        triangles = sum(len(polygon.vertices) - 2 for polygon in  mesh.data.polygons)
+        minRatio = 64.0 / triangles
+        for i in range (1,6):
+            ratios = [0.8, 0.6, 0.4, 0.2, 0.1]
+            new_obj = mesh.copy()
+            new_obj.data = mesh.data.copy()
+            new_obj.animation_data_clear()
+            new_obj.name = mesh.name + "_L{}".format(i)
+            bpy.context.collection.objects.link(new_obj)
+            mod = new_obj.modifiers.new("LOD Decimator", "DECIMATE")
+            mod.ratio = ratios[i - 1] if ratios[i - 1] > minRatio else minRatio
+
+        mesh.name = mesh.name + "_L0"
+        return {'FINISHED'}
+
+
 class TMTKAnimationFixer(bpy.types.Operator):
-    bl_idname = "object.tmtkanimationadfixer"
+    bl_idname = "object.tmtkanimationfixer"
     bl_label = "TMTK Animation Fixer"
 
     @classmethod
@@ -66,18 +97,21 @@ class TMTKSubMenu(bpy.types.Menu):
     def draw(self, context):
         layout = self.layout
         layout.operator(TMTKAnimationFixer.bl_idname)
+        layout.operator(TMTKLODGenerator.bl_idname)
 
 def menu_func(self, context):
     self.layout.menu(TMTKSubMenu.bl_idname)
 
 def register():
     bpy.utils.register_class(TMTKAnimationFixer)
+    bpy.utils.register_class(TMTKLODGenerator)
     bpy.utils.register_class(TMTKSubMenu)
     bpy.types.VIEW3D_MT_object.append(menu_func)
 
 def unregister():
     bpy.utils.unregister_class(TMTKAnimationFixer)
     bpy.utils.unregister_class(TMTKSubMenu)
+    bpy.utils.unregister_class(TMTKLODGenerator)
     bpy.types.VIEW3D_MT_object.remove(menu_func)
 
 if __name__ == "__main__":
