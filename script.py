8103d05db50e088130fa6248c09a81d803b2febc
Gordin508 <5062634+Gordin508@users.noreply.github.com>
2022-10-12 15:19:26 +0200

    Switch to matrix math

--- script.py
+++ script.py
@@ -1,17 +1,7 @@
 import bpy
 from mathutils import Vector
 from mathutils import Matrix
-
-def get_override(area_type, region_type):
-    for area in bpy.context.screen.areas:
-        if area.type == area_type:
-            for region in area.regions:
-                if region.type == region_type:
-                    override = {'area': area, 'region': region}
-                    return override
-    #error message if the area or region wasn't found
-    raise RuntimeError("Wasn't able to find", region_type," in area ", area_type,
-                        "\n Make sure it's open while executing script.")
+import math
 
 class TMTKAnimationAdjuster(bpy.types.Operator):
     bl_idname = "object.tmtkanimationadjuster"
@@ -44,30 +34,12 @@ class TMTKAnimationAdjuster(bpy.types.Operator):
     def editArmature(self, armature : bpy.types.Object):
         assert(bpy.context.active_object.type == "ARMATURE")
         bpy.ops.object.mode_set(mode="EDIT")
-        bpy.ops.armature.select_all(action='SELECT')
-        originalPivot = bpy.context.scene.tool_settings.transform_pivot_point
-        originalCursorPos = bpy.context.scene.cursor.location
-        bpy.context.scene.cursor.location = (0,0,0)
-        bpy.context.scene.tool_settings.transform_pivot_point = 'CURSOR'
-        #we need to override the context of our operator
-        override = get_override( 'VIEW_3D', 'WINDOW' )
-
-        bpy.ops.transform.rotate(override, value=-1.5708, orient_axis='X', orient_type='GLOBAL',
-            orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)),
-            orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=False,
-            use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1,
-            use_proportional_connected=False, use_proportional_projected=False, snap=False,
-            snap_elements={'VERTEX'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=False,
-            use_snap_edit=False, use_snap_nonedit=False, use_snap_selectable=False)
-        bpy.ops.transform.resize(override, value=(100, 100, 100), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)),
-            orient_matrix_type='GLOBAL', mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH',
-            proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, snap=False,
-            snap_elements={'VERTEX'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=False,
-            use_snap_edit=False, use_snap_nonedit=False, use_snap_selectable=False)
-
-        bpy.context.scene.cursor.location = originalCursorPos
-        bpy.context.scene.tool_settings.transform_pivot_point = originalPivot
-
+        bones = armature.data.edit_bones;
+        HALF_PI = math.pi / 2.0
+        for bone in bones:
+            transformMatrix = Matrix.Scale(100.0, 4) @ Matrix.Rotation(-HALF_PI, 4, 'X')
+            bone.transform(transformMatrix)
+        bpy.ops.object.mode_set(mode="OBJECT")
 
 def menu_func(self, context):
     self.layout.operator(TMTKAnimationAdjuster.bl_idname, text=TMTKAnimationAdjuster.bl_label)
